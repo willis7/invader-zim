888b7a1e0cc69a5bce746de9eecb4bd21598d307
willis7 <sion5@hotmail.co.uk>
2015-02-18 22:39:42 +0000

    Set up the controller with Spring

--- src/main/groovy/zim/controller/InvasionController.groovy
+++ src/main/groovy/zim/controller/InvasionController.groovy
@@ -1,23 +1,27 @@
 package zim.controller
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
 import zim.domain.Quote
 import zim.repository.QuoteRepository
 
 /**
  * Created by willis7 on 18/02/15.
  */
+@Controller
 class InvasionController {
 
+    @Autowired
     QuoteRepository repository
 
-    /**
-     * Quote
-     * @param name
-     * @return quote
-     */
-    ResponseEntity<Quote> quote(String name) {
+    @RequestMapping("/invader/{name}")
+    @ResponseBody
+    ResponseEntity<Quote> quote(@PathVariable String name) {
         new ResponseEntity<Quote>(repository.findByName(name), HttpStatus.OK)
     }
 }
